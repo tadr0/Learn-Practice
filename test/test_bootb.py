ce6c38b6715eb55ceef9b62857337ea8917258d3
Thomas A.D. Riley <tom.a.d.riley@gmail.com>
2017-10-27 22:17:19 -0400

    clean up errors from noob git commit

--- test/test_bootb.py
+++ test/test_bootb.py
@@ -6,26 +6,32 @@ Created on Tue Oct 24 13:07:58 2017
 @author: triley
 """
 
-
+import os
 import subprocess
+import sys
 
+#src_dir = os.path.dirname(os.path.abspath('./src/bootb/update_bootb.py'))
+#if not src_dir in sys.path:
+#    sys.path.append(src_dir)
+#print(sys.path)
 
-from update_bootb import (get_blkids, get_proc_mounts)
+# run pytest from top of repo
+from src.bootb.update_bootb import get_blkids, get_proc_mounts
 
 
 def test_get_proc_mounts():
-    """ At least something works. """
-    assert '/' in get_proc_mounts().values()
-    assert '/boot/efi' in get_proc_mounts().values()
-    assert '/dev/sda1' in get_proc_mounts().keys()
-    assert '/dev/sda2' in get_proc_mounts().keys()
+    mounts = get_proc_mounts()
+    # some sanity checks we can get independently from get_blkids
+    assert '/' in mounts.values()
+    assert '/boot/efi' in mounts.values()
+    # tests that need apriori knowledge (not generally useful but fine for me)
+    assert '/dev/sda1' in mounts.keys()
+    assert '/dev/sda2' in mounts.keys()
 
 
 def test_get_blkids():
-
-    """ some sanity checks we can get independently from get_blkids """
     sources = get_blkids()
-
+    # independent
     # is each partition from blkid also in /proc/partitions ?
     result = subprocess.run(
         'cat /proc/partitions',
@@ -38,3 +44,4 @@ def test_get_blkids():
         print(part)
         part = part[5:9]  # strip off /dev/
         assert part in response
+    # tests that need apriori knowledge
