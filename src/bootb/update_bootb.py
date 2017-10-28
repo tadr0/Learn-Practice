3f03cb7f2a2093dfe42eb4bf002bcf4021e00416
Thomas A.D. Riley <tom.a.d.riley@gmail.com>
2017-10-28 10:04:35 -0400

    add a few utility functions

--- src/bootb/update_bootb.py
+++ src/bootb/update_bootb.py
@@ -57,3 +57,85 @@ def get_blkids():
                 mount_point=mount)
 
     return blkids
+
+
+def find_sources():
+    """ Find out where to copy from and return a reverse lookup, sources. """
+    mounts = get_proc_mounts()  # indexed by '/dev/sdxx/
+    all_parts = get_blkids()  # indexed by '/dev/sdxx/
+    sources = {}
+
+    for part in mounts.keys():
+        for mount in ['/', '/boot/efi']:
+            if mounts[part] == mount:
+                sources[mount] = all_parts[part]
+    # BIST: Make sure we have two source partitions with correct mounts.
+    k = sources.keys()
+    assert len(k) == 2, 'wtf: Failed to find sources.'
+    for mnt in k:
+        msg = 'wtf: sources mis-mapped'
+        assert sources[mnt].mount_point == mnt, msg
+    assert sources['/boot/efi'].ptype == 'vfat'
+    return sources
+
+
+def find_dest_disk():
+    potentials = get_blkids()  # idexed by '/dev/sdxx'
+    mounts = get_proc_mounts()  # idexed by '/dev/sdxx'
+
+    efi_candidates = []
+    root_candidates = []
+    # Find available vfat partitions for EFI.
+    efi_disks = []
+    for p in potentials.keys():  # exclude mounted EFI
+
+        if potentials[p].ptype == 'vfat' and ('/boot/efi' not in mounts[p]):
+            efi_candidates += [potentials[p]]
+            # '/' has to be on the same disk as EFI.
+            efi_disks += [p[5:8]]  # disk canddates
+
+    for p in potentials.keys():
+        # print(p)
+        disk = p[5:8]
+        if disk in efi_disks:
+            # print(f'checking {p}')
+            if (p not in mounts.keys()):  # p not mounted is a good sign
+                # print(f'{p} not mounted')
+                root_candidates[p] = potentials[p]
+            else:  # if mounted, cant be '/'
+                # print(f'mounts[{p}] = {mounts[p]}')
+                if potentials[p].ptype != 'vfat':
+                    root_candidates += [potentials[p]]
+
+    if len(root_candidates) == 1:
+        return {'/boot/efi': efi_candidates[0], '/': root_candidates[0]}
+#    elif len(root_candidates) == 0:
+#        assert False, 'No install candidates found'
+#    else:
+#        assert False, 'TODO fix me later by allowing choice from candidates'
+
+
+
+def check_mount_point(mount_point):
+    if not os.path.isdir(mount_point):
+        try:
+            os.mkdir(mount_point)
+        except OSError as e:
+            if e.errno == errno.EEXIST:  # seems impossible
+                pass
+            elif e.errno == errno.EACCES:
+                assert False, 'Must be run as root user'
+            elif e.errno == 2:  # TODO look up non-numeric
+                # because we got crap for mount_point ?
+                assert False, f'{mount_point} not found making mount_point'
+            else:
+                pprint(e)
+                assert False, f'wtf checking mount_point {mount_point}'
+        # print(f'created mount point {mount_point}')
+        return
+    else:
+        pass
+
+
+if __name__ == '__main__':
+    pprint(find_dest_disk())
--- test/test_bootb.py
+++ test/test_bootb.py
@@ -6,17 +6,17 @@ Created on Tue Oct 24 13:07:58 2017
 @author: triley
 """
 
+# usage: run pytest from top of repo with no parameters eg.
+# ~/src/Learn-Practice$ pytest
+
 import os
 import subprocess
 import sys
+import uuid
 
-#src_dir = os.path.dirname(os.path.abspath('./src/bootb/update_bootb.py'))
-#if not src_dir in sys.path:
-#    sys.path.append(src_dir)
-#print(sys.path)
 
-# run pytest from top of repo
-from src.bootb.update_bootb import get_blkids, get_proc_mounts
+from src.bootb.update_bootb import (get_blkids, check_mount_point,
+    find_sources, get_proc_mounts, find_dest_disk)
 
 
 def test_get_proc_mounts():
@@ -25,8 +25,8 @@ def test_get_proc_mounts():
     assert '/' in mounts.values()
     assert '/boot/efi' in mounts.values()
     # tests that need apriori knowledge (not generally useful but fine for me)
-    assert '/dev/sda1' in mounts.keys()
-    assert '/dev/sda2' in mounts.keys()
+    assert mounts['/dev/sda1'] == '/boot/efi'
+    assert mounts['/dev/sda2'] == '/'
 
 
 def test_get_blkids():
@@ -45,3 +45,26 @@ def test_get_blkids():
         part = part[5:9]  # strip off /dev/
         assert part in response
     # tests that need apriori knowledge
+
+
+def test_find_sources():
+    mounts = find_sources()
+    # tests that need apriori knowledge
+    assert mounts['/dev/sda1'] == '/boot/efi'
+    assert mounts['/dev/sda2'] == '/'
+
+
+def test_find_dest_disk():
+    dest = find_dest_disk()
+    # tests that need apriori knowledge
+    assert dest['/boot/efi'] == ['/dev/sdc1']
+    assert dest['/'] == '/dev/sda2'
+
+
+def test_mount_point_new():
+    """ check_mount_point should make the dir if it's not there
+        Gamble on the non-existence of a weird directory name. """
+    dir_name = f'/tmp/dir_{uuid.uuid1()}'
+    check_mount_point(dir_name)
+    assert os.path.isdir(dir_name)
+    os.rmdir(dir_name)  # context manager here ?
