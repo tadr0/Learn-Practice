3d4acc1b1841b3929f9f403356cd98d3044dc179
Thomas A.D. Riley <tom.a.d.riley@gmail.com>
2017-10-29 11:57:28 -0400

    abstarct partions with a class, allow testing of more cases than 1 laptop can provide

--- test_bootb.py
+++ test_bootb.py
@@ -11,12 +11,14 @@ Created on Tue Oct 24 13:07:58 2017
 
 import os
 import subprocess
-import sys
+# import sys
 import uuid
+from pprint import pprint
 
 
-from src.bootb.update_bootb import (get_blkids, check_mount_point,
-    find_sources, get_proc_mounts, find_dest_disk)
+from update_bootb import (
+        DiskPart, PartitionTable,
+        find_source_disk, find_dest_disk)
 
 
 def test_get_proc_mounts():
@@ -29,12 +31,10 @@ def test_get_proc_mounts():
     assert mounts['/dev/sda2'] == '/'
 
 
-def test_get_blkids():
-    sources = get_blkids()
+def test_get_blkids(part_table):
+    sources = part_table.partitions
     # independent
-    # is each partition from blkid also in /proc/partitions ?
     result = subprocess.run(
-        'cat /proc/partitions',
         stdout=subprocess.PIPE,
         shell=True)
     result.check_returncode()
@@ -47,24 +47,79 @@ def test_get_blkids():
     # tests that need apriori knowledge
 
 
-def test_find_sources():
-    mounts = find_sources()
+def test_find_source_disk():
+    mounts = get_proc_mounts()
     # tests that need apriori knowledge
     assert mounts['/dev/sda1'] == '/boot/efi'
     assert mounts['/dev/sda2'] == '/'
 
 
 def test_find_dest_disk():
-    dest = find_dest_disk()
-    # tests that need apriori knowledge
-    assert dest['/boot/efi'] == ['/dev/sdc1']
-    assert dest['/'] == '/dev/sda2'
+    # most typical
+    def any_part(name, ptype, mount_point, fake_data):
+        return DiskPart(
+            dev=None,
+            uuid=None,
+            ptype=None,
+            puuid=None,
+            mount_point=None)
+
+    # make all the combinations without having to check them by hand
+    # partitions = {}
+    disk_states = {}
+    for name in ['sda', 'sdb', 'sdc']:
+        n = name[2]
+        for part_number in ['1', '2']:
+            full_name = f'/dev/{name}{part_number}'
+            for ptype in ['vfat', 'junk', 'ext4']:
+                t = ptype[0]
+                for m, mount in {
+                        'u': None,
+                        'm': f'/mnt/{name}{part_number}',
+                        'r': '/',
+                        'b': '/boot/efi'}.items():
+                    idx = f'sd{n}{part_number}_{t}_{m}'
+                    test_part = DiskPart(
+                        dev=full_name,
+                        ptype=ptype,
+                        mount_point=mount)
+                    disk_states[idx] = test_part
+                    # print(idx, full_name, ptype, mount, None)
+
+    test_casess = [  # correct answers for each test case
+        # efi mount, root mount,  list [diskpartition_ptype_mount]
+        ('sdb1', 'sdb2', 'sda1_v_b sda2_e_r sdb1_v_u sdb2_e_u'.split()),
+        ('sdb1', 'sdb2', 'sda1_v_b sda2_e_r sdb1_v_u sdb2_e_m'.split()),
+
+        # from usb back to HDD
+        ('sda1', 'sda2', 'sdb1_v_b sdb2_e_r sda1_v_u sda2_e_m'.split()),
+
+        # list order shouldn't matter
+        ('sda1', 'sda2', 'sda1_v_b sda2_e_r sdb1_v_u sdb2_e_m'.split()),
+
+        # and more later
+        ]
+
+    for case in test_casess:
+        potentials = [disk_states[idx] for idx in case[2]]
+        # print(potentials)
+        try:
+            dest = find_dest_disk(potentials)
+            assert dest['/boot/efi'] == f'/dev/{case[0]}'
+            assert dest['/'] == f'/dev/{case[1]}'
+        except:
+            if case[1] is None:
+                pass
 
 
-def test_mount_point_new():
-    """ check_mount_point should make the dir if it's not there
-        Gamble on the non-existence of a weird directory name. """
-    dir_name = f'/tmp/dir_{uuid.uuid1()}'
-    check_mount_point(dir_name)
-    assert os.path.isdir(dir_name)
-    os.rmdir(dir_name)  # context manager here ?
+#def test_mount_point_new():
+#    """ check_mount_point should make the dir if it's not there
+#        Gamble on the non-existence of a weird directory name. """
+#    dir_name = f'/tmp/dir_{uuid.uuid1()}'
+#    check_mount_point(dir_name)
+#    assert os.path.isdir(dir_name)
+#    os.rmdir(dir_name)  # context manager here ?
+
+
+if __name__ == '__main__':
+    test_find_dest_disk()
--- update_bootb.py
+++ update_bootb.py
@@ -7,62 +7,176 @@ Created on Mon Oct 23 17:34:02 2017
 """
 
 
-import os
-import errno
 import re
-from collections import namedtuple
 # import sys
 # from subprocess import check_call, run
 import subprocess
 from pprint import pprint
 
-
-def get_proc_mounts():
-    """ filter mounted for disks """
-    mounts = {}  # mount points indexed by '/dev/sdxx'
-    with open('/proc/mounts') as f:
-        lines = f.readlines()
-
-    for line in lines:
-        if '/dev/' in line[:5]:  # then it's a disk device
-            line = line.split()
-            mounts[line[0]] = line[1]
-    return mounts
+import os
+import errno
 
 
-def get_blkids():
-    re_uuid = re.compile(b'UUID="(.*?)"')
-    re_type = re.compile(b'TYPE="(.*?)"')
-    re_puuid = re.compile(b'PARTUUID="(.*?)"')
-    DiskPart = namedtuple('DiskPart', 'dev uuid ptype puuid mount_point')
+class DiskPart(object):
+    """  isolate hardware from everything else, abstract partitions
+    """
 
-    mounts = get_proc_mounts()
-    blkids = {}  # device info indexed by '/dev/sdxx'
-    result = subprocess.run('blkid', stdout=subprocess.PIPE)
-    result.check_returncode()
-    lines = result.stdout.split(b'\n')
-    for line in lines:
-        if b'/dev/' in line:  # ignore empty lines
-            p = line.split(b':')
-            dev = p[0].strip().decode("utf-8")  # python3 string, no blanks
-            if dev in mounts.keys():
-                mount = mounts[dev]
-            else:
-                mount = None
-            blkids[dev] = DiskPart(
-                dev=dev,  # keep dev info in the DiskPart instance
-                uuid=re_uuid.search(p[1]).group(1).decode("utf-8"),
-                ptype=re_type.search(p[1]).group(1).decode("utf-8"),
-                puuid=re_puuid.search(p[1]).group(1).decode("utf-8"),
-                mount_point=mount)
+    def __init__(
+            self,
+            dev='',
+            uuid='',
+            ptype='',
+            puuid='',
+            mount_point='',
+            fake_data=None):
 
-    return blkids
+        self.dev = dev
+        self.disk = dev[5:8],
+        self.part_num = dev[8],
+        self.uuid = uuid
+        self.ptype = ptype
+        self.puuid = puuid
+        self.mount_point = mount_point
+        self.fake_data = fake_data
+
+    def __str__(self):
+        s = []
+        s += ['  H']
+        s += [self.dev if self.dev else ' '*9]
+#        s += [self.ptype if self else ' '*4]
+        s += [self.ptype[:4] if self.ptype else ' '*4]
+        s += [self.uuid[:8] if self.uuid else ' '*8]
+        s += [self.mount_point if self.mount_point else 'None']
+        return ' '.join(s)
+
+    def __repr__(self):
+        self.__str__()  # cheat
 
 
-def find_sources():
+class PartitionTable(object):
+    """ Abstract Partition Table
+
+        really info about table and maybe setup for MagicMock?
+    """
+
+    def __init__(self):
+        # mount points, partitions indexed by '/dev/sdxx'
+        self.mounts = {}
+        self.partitions = self.get_blkids()
+
+    def __str__(self):
+        s = ['Partition Table:']
+        for name, part in self.partitions.items():
+            s += [part.__str__()]
+        return '\n'.join(s)
+
+    def __repr__(self):
+        self.__str__()
+
+    def add_mok_partition():
+        pass
+
+    def get_blkids(self):
+        """ This seems to work fine for non-root user, provided that you poke
+            it first with a 'sudo blkid' at the command line once. Hmm ...
+        """
+        re_uuid = re.compile(b'UUID="(.*?)"')
+        re_type = re.compile(b'TYPE="(.*?)"')
+        re_puuid = re.compile(b'PARTUUID="(.*?)"')
+
+        mounts = self.get_proc_mounts()
+        blkids = {}  # device info indexed by '/dev/sdxx'
+        result = subprocess.run('blkid', stdout=subprocess.PIPE)
+        result.check_returncode()
+        lines = result.stdout.split(b'\n')
+        for line in lines:
+            if b'/dev/' in line:  # ignore empty lines
+                p = line.split(b':')
+                dev = p[0].strip().decode("utf-8")  # python3 string, no blanks
+                uuid_m = re_uuid.search(p[1])
+                ptype_m = re_type.search(p[1])
+                puuid_m = re_puuid.search(p[1])
+                if dev in mounts.keys():
+                    mount = mounts[dev]
+                else:
+                    mount = None
+                blkids[dev] = DiskPart(
+                    dev=dev,  # keep dev info in the DiskPart instance
+                    uuid=uuid_m.group(1).decode("utf-8") if uuid_m else None,
+                    ptype=ptype_m.group(1).decode("utf-8") if ptype_m else None,
+                    puuid=puuid_m.group(1).decode("utf-8") if puuid_m else None,
+                    mount_point=mount,
+                    fake_data=None)
+                # print(blkids[dev])
+        return blkids
+
+    def get_proc_mounts(self):
+        """ filter mounted for disks """
+        with open('/proc/mounts') as f:
+            lines = f.readlines()
+        for line in lines:
+            if '/dev/' in line[:5]:  # then it's a disk device
+                line = line.split()
+                self.mounts[line[0]] = line[1]
+        return self.mounts
+
+    def check_mount_point(self, mount_point):
+        if not os.path.isdir(mount_point):
+            try:
+                os.mkdir(mount_point)
+            except OSError as e:
+                if e.errno == errno.EEXIST:  # seems impossible
+                    pass
+                elif e.errno == errno.EACCES:
+                    raise Exception('Must be run as root user')
+                elif e.errno == 2:  # TODO look up non-numeric
+                    # because we got crap for mount_point ?
+                    raise Exception(
+                            f'{mount_point} not found making mount_point')
+                else:
+                    pprint(e)
+                    raise Exception(f'wtf checking mount_point {mount_point}')
+            # print(f'created mount point {mount_point}')
+            return
+        else:
+            pass
+
+    def _mount_partition(self, mount_point):
+        """ /bin/mount requires root priveledge
+        """
+
+        self.check_mount_point(mount_point)
+
+        # mounted allready ?
+        proc_mounts = self.get_proc_mounts()
+        cmd = ['/bin/mount', self.name, self.mount_point]
+        msg = f'wtf mounting partition {partition} at {mount_point}'
+        if self.name not in proc_mounts:
+            error_ret = subprocess.check_call(cmd)
+        if proc_mounts[self.name] != mount_point:
+            error_ret = subprocess.check_call(cmd)
+            if error_ret:
+                raise Exception(msg)
+        if self.get_proc_mounts()[self.name] == mount_point:
+            self.mount_point = mount_point  # update data
+        else:
+            raise Exception(msg)
+
+    def mok_mount(self, mount_point):
+
+        raise Exception('not implemented yet')
+
+    def mount_partition(self, mount_point):   # desired mount point
+        if os.getuid() == 0:
+            self._mount_partition(self, mount_point)
+        else:
+            self.mok_mount(mount_point)
+
+
+def find_source_disk(partition_table):
     """ Find out where to copy from and return a reverse lookup, sources. """
-    mounts = get_proc_mounts()  # indexed by '/dev/sdxx/
-    all_parts = get_blkids()  # indexed by '/dev/sdxx/
+    mounts = partition_table.mounts  # indexed by '/dev/sdxx/
+    all_parts = partition_table.partitions  # indexed by '/dev/sdxx/
     sources = {}
 
     for part in mounts.keys():
@@ -79,63 +193,47 @@ def find_sources():
     return sources
 
 
-def find_dest_disk():
-    potentials = get_blkids()  # idexed by '/dev/sdxx'
-    mounts = get_proc_mounts()  # idexed by '/dev/sdxx'
+def find_dest_disk(potentials):
+    print('find_dest_disk:', len(potentials))
+    # potentials = all_partitions.values()  # [DiskPart] list
+    # [DiskPart] list
+    efi_candidates = []  # [DiskPart] list
 
-    efi_candidates = []
-    root_candidates = []
     # Find available vfat partitions for EFI.
-    efi_disks = []
-    for p in potentials.keys():  # exclude mounted EFI
 
-        if potentials[p].ptype == 'vfat' and ('/boot/efi' not in mounts[p]):
-            efi_candidates += [potentials[p]]
-            # '/' has to be on the same disk as EFI.
-            efi_disks += [p[5:8]]  # disk canddates
+    for partition in potentials:  # exclude mounted EFI
+        if partition.ptype == 'vfat' and (
+                '/boot/efi' not in [partition.mount_point]):
+            efi_candidates += [partition]
 
-    for p in potentials.keys():
-        # print(p)
-        disk = p[5:8]
-        if disk in efi_disks:
-            # print(f'checking {p}')
-            if (p not in mounts.keys()):  # p not mounted is a good sign
-                # print(f'{p} not mounted')
-                root_candidates[p] = potentials[p]
-            else:  # if mounted, cant be '/'
-                # print(f'mounts[{p}] = {mounts[p]}')
-                if potentials[p].ptype != 'vfat':
-                    root_candidates += [potentials[p]]
+    # The new '/' has to be put on the same disk as the new EFI.
+    for efi_part in efi_candidates:
+        efi_disk = partition.dev[5:8]  # 'sd?'
 
+        root_candidates = []
+        for p in potentials:
+            if p.ptype != 'vfat' and efi_disk in p.dev:
+                root_candidates += [(efi_part, partition)]
+    print('find_dest_disk: count =', len(root_candidates))
     if len(root_candidates) == 1:
-        return {'/boot/efi': efi_candidates[0], '/': root_candidates[0]}
-#    elif len(root_candidates) == 0:
-#        assert False, 'No install candidates found'
-#    else:
-#        assert False, 'TODO fix me later by allowing choice from candidates'
-
-
-
-def check_mount_point(mount_point):
-    if not os.path.isdir(mount_point):
-        try:
-            os.mkdir(mount_point)
-        except OSError as e:
-            if e.errno == errno.EEXIST:  # seems impossible
-                pass
-            elif e.errno == errno.EACCES:
-                assert False, 'Must be run as root user'
-            elif e.errno == 2:  # TODO look up non-numeric
-                # because we got crap for mount_point ?
-                assert False, f'{mount_point} not found making mount_point'
-            else:
-                pprint(e)
-                assert False, f'wtf checking mount_point {mount_point}'
-        # print(f'created mount point {mount_point}')
-        return
+        return {'/boot/efi': efi_part[0], '/': partition[0]}
+    elif len(root_candidates) == 0:
+        raise Exception('No install candidates found')
+    elif len(root_candidates) > 1:
+        raise Exception('TODO fix later, choose from root_candidates')
     else:
-        pass
+        raise Exception('wtf in find_dest_disk')
 
 
 if __name__ == '__main__':
-    pprint(find_dest_disk())
+    part_table = PartitionTable()
+
+    print(part_table)
+
+    # print(find_source_disk(part_table))
+    source_disk = find_source_disk(part_table)
+    # pprint(source_disk)
+    dest_disk = part_table.get_blkids().values()
+    # print(dest_disk)
+
+    pass
